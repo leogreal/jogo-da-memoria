2dda0f7c430dd2d0ff5510faacda5e4cce2785cf
Leonardo <leogreal@gmail.com>
2019-01-21 22:43:26 +0000

    Melhorando a leitura, template string

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
-const $icon = "<img class='icon' src='img/icon-collabcode.png' alt='Gueio mascode da CollabCode' />";
+const $icon = `
+  <img
+    class='icon'
+    src='img/icon-collabcode.png'
+    alt='Gueio mascode da CollabCode'
+  />
+`;
 
 $memoryCard.classList.add("memory-card");
 
