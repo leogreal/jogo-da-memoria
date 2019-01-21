11d9c5da130bdc05935781279d930a9721e430b8
Leonardo <leogreal@gmail.com>
2019-01-21 22:39:25 +0000

    Sofrendo para aprender o insertAdjacentHTML

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $icon = "<img class='icon' src='img/icon-collabcode.png' alt='Gueio mascode da CollabCode' />";
 
 $memoryCard.classList.add("memory-card");
 
 $root.insertBefore($memoryCard, null);
+
+$memoryCard.insertAdjacentHTML("afterbegin", $icon);
