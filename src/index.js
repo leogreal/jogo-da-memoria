560ff2c6de9923e8805e87f504cf651cc6959ea8
Leonardo <leogreal@gmail.com>
2019-01-22 23:30:58 +0000

    Criando componente com JavaScript

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $memoryCardFront = document.createElement("article");
+
+const $iconCollab = `
+  <img
+    class='icon'
+    src='img/icon-collabcode.png'
+    alt='Gueio mascode da CollabCode'
+  />
+`;
+
+const $iconC = `
+  <img
+    class='icon'
+    src='img/icon-js.png'
+    alt='Icone da linguagem'
+  />
+`;
 
 $memoryCard.classList.add("memory-card");
-
 $root.insertBefore($memoryCard, null);
 
-$memoryCard.insertAdjacentHTML("afterbegin", icon("img/icon-collabcode.png", "Gueio mascode da CollabCode"));
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, null);
 
-function icon(mySrc, myAlt) {
-  return `
-    <img
-      class='icon'
-      src='${mySrc}'
-      alt='${myAlt}'
-    />
-  `;
-}
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
