05c4da14350c92fed610efd7013f20beab7ccfda
Leonardo <leogreal@gmail.com>
2019-01-22 23:15:26 +0000

    Criando a bola pelo CSS

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
-const $upsetMemoryCard = document.createElement("article");
 
 $memoryCard.classList.add("memory-card");
-$upsetMemoryCard.classList.add("memory-card");
-$upsetMemoryCard.classList.add("-upset");
 
-$root.insertBefore($upsetMemoryCard, null);
 $root.insertBefore($memoryCard, null);
 
-$upsetMemoryCard.insertAdjacentHTML("afterbegin", icon("img/icon-js.png", "JavaScript"));
 $memoryCard.insertAdjacentHTML("afterbegin", icon("img/icon-collabcode.png", "Gueio mascode da CollabCode"));
 
 function icon(mySrc, myAlt) {
