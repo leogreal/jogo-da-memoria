08f8a6791a99ac9cfd8fc46c9a5f8cc4619adf27
Leonardo <leogreal@gmail.com>
2019-01-23 21:01:02 +0000

    [022] - Desafio Básico + Intermediário + Avançado

--- src/index.js
+++ src/index.js
@@ -1,30 +1,45 @@
 const $root = document.querySelector("#root");
-const $memoryCard = document.createElement("article");
-const $memoryCardFront = document.createElement("article");
+const $cardContainer = document.createElement("section");
 
-const $iconCollab = `
+$root.insertBefore($cardContainer, null);
+$cardContainer.classList.add("card-container");
+
+
+const $cards = [
+  { src: 'img/icon-collabcode.png', alt: 'Gueio mascode da CollabCode', front: false },
+  { src: 'img/icon-bug.png', alt: 'card bug', front: true },
+  { src: 'img/icon-c.png', alt: 'card c', front: true },
+  { src: 'img/icon-cabeca.png', alt: 'card cabeça', front: true },
+  { src: 'img/icon-celular.png', alt: 'card celular', front: true },
+  { src: 'img/icon-java.png', alt: 'card java', front: true },
+  { src: 'img/icon-js.png', alt: 'card java script', front: true },
+  { src: 'img/icon-php.png', alt: 'card php', front: true },
+  { src: 'img/icon-responsivo.png', alt: 'card responsivo', front: true },
+  { src: 'img/icon-settings.png', alt: 'card settings', front: true },
+  { src: 'img/icon-woman.png', alt: 'card woman', front: true }
+];
+
+$cards.forEach(card => createCard(card));
+
+function createCard(card){
+  const $memoryCard = document.createElement("article");
+  $cardContainer.insertBefore($memoryCard, null);
+  
+  $memoryCard.classList.add("memory-card");
+  
+  if(card.front){
+    $memoryCard.classList.add("-front");
+  }
+
+  $memoryCard.insertAdjacentHTML("afterbegin", createIcon(card));
+}
+
+function createIcon(card){
+  return `
   <img
     class='icon'
-    src='img/icon-collabcode.png'
-    alt='Gueio mascode da CollabCode'
+    src='${card.src}'
+    alt='${card.alt}'
   />
 `;
-
-const $iconC = `
-  <img
-    class='icon'
-    src='img/icon-js.png'
-    alt='Icone da linguagem'
-  />
-`;
-
-$memoryCard.classList.add("memory-card");
-$root.insertBefore($memoryCard, null);
-
-$memoryCardFront.classList.add("memory-card");
-$memoryCardFront.classList.add("-front");
-$root.insertBefore($memoryCardFront, null);
-
-$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
-
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+}
