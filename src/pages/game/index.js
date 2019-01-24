fb153985df5c8f2ba9d1ee2c0cb60131787d9919
Leonardo <leogreal@gmail.com>
2019-01-24 23:06:19 +0000

    [023] - Desafio Único

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -6,3 +6,17 @@ createMemoryCard();
 createMemoryCard();
 createMemoryCardFront();
 createMemoryCardFront();
+
+document.querySelectorAll(".memory-card").forEach($card => {
+  $card.addEventListener("click", e => {
+    e.preventDefault();
+    console.log("Oi, eu sou um card!");
+  });
+});
+
+$wrapCards.addEventListener("click", e => {
+  const { tagName } = e.target;
+  if (tagName === "ARTICLE" || tagName === "IMG") {
+    console.log("Oi, eu sou um card borbulhando!");
+  }
+});
