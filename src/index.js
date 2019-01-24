671994c58de2553cbfc91de90b4f1e833c87059c
Leonardo <leogreal@gmail.com>
2019-01-24 21:16:45 +0000

    Código repetido

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
+const $memoryCard1 = document.createElement("article");
 const $memoryCardFront = document.createElement("article");
 
 const $iconCollab = `
@@ -18,13 +19,15 @@ const $iconC = `
   />
 `;
 
-$memoryCard.classList.add("memory-card");
-$root.insertBefore($memoryCard, null);
-
 $memoryCardFront.classList.add("memory-card");
 $memoryCardFront.classList.add("-front");
 $root.insertBefore($memoryCardFront, null);
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
 
+$memoryCard.classList.add("memory-card");
+$root.insertBefore($memoryCard, null);
 $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
 
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+$memoryCard1.classList.add("memory-card");
+$root.insertBefore($memoryCard1, null);
+$memoryCard1.insertAdjacentHTML("afterbegin", $iconCollab);
