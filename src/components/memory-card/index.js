3778196640cb55e4b315061f8502994fdcfa0066
Leonardo <leogreal@gmail.com>
2019-01-24 21:38:11 +0000

    Isolar código do memory card front

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,3 +1,5 @@
+const $root = document.querySelector("#root");
+
 function createMemoryCard() {
   const $memoryCard = document.createElement("article");
 
@@ -12,4 +14,21 @@ function createMemoryCard() {
   $memoryCard.classList.add("memory-card");
   $root.insertBefore($memoryCard, null);
   $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+}
+
+function createMemoryCardFront() {
+  const $memoryCardFront = document.createElement("article");
+
+  const $iconC = `
+    <img
+      class='icon'
+      src='img/icon-js.png'
+      alt='Icone da linguagem'
+    />
+  `;
+
+  $memoryCardFront.classList.add("memory-card");
+  $memoryCardFront.classList.add("-front");
+  $root.insertBefore($memoryCardFront, null);
+  $memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
 }
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,18 +1,8 @@
-const $root = document.querySelector("#root");
-const $memoryCardFront = document.createElement("article");
-
 createMemoryCard();
 createMemoryCard();
-
-const $iconC = `
-  <img
-    class='icon'
-    src='img/icon-js.png'
-    alt='Icone da linguagem'
-  />
-`;
-
-$memoryCardFront.classList.add("memory-card");
-$memoryCardFront.classList.add("-front");
-$root.insertBefore($memoryCardFront, null);
-$memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCard();
+createMemoryCardFront();
+createMemoryCardFront();
