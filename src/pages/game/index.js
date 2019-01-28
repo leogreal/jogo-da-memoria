3ae82f7dc84c418a95cff541eb936edbf8d91711
Leonardo <leogreal@gmail.com>
2019-01-28 22:16:22 +0000

    Adicionando alguns memory cards

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -5,4 +5,12 @@ const $memoryCard = createMemoryCard();
 const $memoryCardFront = createMemoryCardFront();
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
