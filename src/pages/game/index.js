a2a935a3917d91c7ceaddee39986fcb3c0f1ce87
Leonardo <leogreal@gmail.com>
2019-01-28 21:58:52 +0000

    Inserindo o elemento cards-wrapper

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,3 +1,7 @@
-const cardsWrapper = createCardsWrapper();
-const memoryCard = createMemoryCard();
-const memoryCardFront = createMemoryCardFront();
+const $root = document.querySelector("#root");
+
+const $cardsWrapper = createCardsWrapper();
+const $memoryCard = createMemoryCard();
+const $memoryCardFront = createMemoryCardFront();
+
+$root.insertAdjacentHTML("beforeend", $cardsWrapper);
