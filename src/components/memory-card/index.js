019ba1b3b74b70416a26788cfbaa44b5bac0d5e8
Leonardo <leogreal@gmail.com>
2019-01-30 21:23:06 +0000

    um if ternário

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,20 +1,14 @@
-const createMemoryCard = nameClass => {
-  let src = "img/icon-collabcode.png";
-
-  if (nameClass === "-front") {
-    src = "img/icon-js.png";
-  }
-
-  return `
-    <article class="memory-card ${nameClass}">
-      <img
-        class='icon'
-        src='${src}'
-        alt='Gueio mascode da CollabCode'
-        onClick="handleClick()"
-      />
-    </article>
-  `;
-};
+const createMemoryCard = nameClass => `
+  <article class="memory-card ${nameClass}">
+    <img
+      class='icon'
+      src='${
+        nameClass === "-front" ? "img/icon-js.png" : "img/icon-collabcode.png"
+      }'
+      alt='Gueio mascode da CollabCode'
+      onClick="handleClick()"
+    />
+  </article>
+`;
 
 const handleClick = () => console.log("foi");
