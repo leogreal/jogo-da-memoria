6c1cb1f1cbe38ff4986005e1e65c01963e9eb40f
Leonardo <leogreal@gmail.com>
2019-01-30 21:32:50 +0000

    Agora temos um component stateless

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,13 +1,9 @@
-const createMemoryCard = (src, nameClass) => `
+const createMemoryCard = (src, alt, nameClass) => `
   <article class="memory-card ${nameClass}">
     <img
       class='icon'
       src='${src}'
-      alt='${
-        nameClass === "-front"
-          ? "Icone de um livro de JavaScript"
-          : "Gueio mascode da CollabCode"
-      }'
+      alt='${alt}'
       onClick="handleClick()"
     />
   </article>
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,8 +1,15 @@
 const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
-const $memoryCard = createMemoryCard("img/icon-collabcode.png");
-const $memoryCardJS = createMemoryCard("img/icon-js.png", "-front");
+const $memoryCard = createMemoryCard(
+  "img/icon-collabcode.png",
+  "Icone de um livro de JavaScript"
+);
+const $memoryCardJS = createMemoryCard(
+  "img/icon-js.png",
+  "Gueio mascode da CollabCode",
+  "-front"
+);
 
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
 $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
