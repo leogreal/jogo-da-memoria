db259334c0460db21e50c8f6d6b5dc86c1ce93b1
Leonardo <leogreal@gmail.com>
2019-02-04 21:52:02 +0000

    Agora estamos retornando uma function

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,21 +2,26 @@ const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
 
+const createMemoryCard = memoryCard();
+
 const $memoryCardC = createMemoryCard({
   src: "img/icon-c.png",
   alt: "Icone de um livro de C++",
   className: "-front"
 });
+
 const $memoryCardJS = createMemoryCard({
   src: "img/icon-js.png",
   alt: "Icone de um livro de JavaScript",
   className: "-front"
 });
+
 const $memoryCardJava = createMemoryCard({
   src: "img/icon-java.png",
   alt: "Icone de um livro de Java",
   className: "-front"
 });
+
 const $memoryCardPHP = createMemoryCard({
   src: "img/icon-php.png",
   alt: "Icone de um livro de PHP",
