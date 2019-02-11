bd250e8a20c4f701cecf1dfe60adee03529c44da
Leonardo <leogreal@gmail.com>
2019-02-11 21:14:55 +0000

    Temos um bug, bora arrumar?

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -80,12 +80,14 @@ const memoryCard = () => {
 };
 
 const handleClick = $component => {
-  if (qtdActiveMemoryCard < 2) {
-    $component.classList.toggle("-active");
-  }
+  if (!$component.classList.contains("-active")) {
+    if (qtdActiveMemoryCard < 2) {
+      $component.classList.toggle("-active");
+    }
 
-  if (qtdActiveMemoryCard === 1) {
-    checkPair();
+    if (qtdActiveMemoryCard === 1) {
+      checkPair();
+    }
   }
 };
 
