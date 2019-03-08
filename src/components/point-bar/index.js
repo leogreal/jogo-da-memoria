521c98e6d00191eed80d897cbeb22e46be7445c8
Leonardo <leogreal@gmail.com>
2019-03-08 00:49:24 +0000

    Falta vida, falta CSS

--- src/components/point-bar/index.js
+++ src/components/point-bar/index.js
@@ -1,10 +1,33 @@
 const pointBar = (function() {
   const module = {};
 
+  module.style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+    $style.textContent = `
+      .point-bar {
+        background-color: #3a4042;
+        height: 40px;
+        text-align: center;
+      }
+
+      .point-bar > .number {
+        color: #FFF;
+        line-height: 40px;
+        font-family: 'Comfortaa', sans-serif;
+        font-weight: bold;
+      }
+    `;
+
+    $head.insertBefore($style, null);
+  };
+
   module.create = () => {
+    module.style();
+
     return `
-      <header>
-        <span>1</span>
+      <header class="point-bar">
+        <span class="number">0</span>
       </header>
     `;
   };
