949b6368efec2545938f784fecb82d26f509d861
Leonardo <leogreal@gmail.com>
2019-03-08 00:23:33 +0000

    Estrutura e organização do point-bar

---
+++ src/components/point-bar/index.js
@@ -0,0 +1,15 @@
+const pointBar = (function() {
+  const module = {};
+
+  module.create = () => {
+    return `
+      <header>
+        <span>1</span>
+      </header>
+    `;
+  };
+
+  return {
+    create: module.create
+  };
+})();
